3f9876553eb9388f30c54381b2e31ca8790ac758
YevheniiaMazur <yevheniia.mazur@valor-software.com>
2019-06-30 21:11:15 +0300

    add page objects

---
+++ cypress/support/deals.po.ts
@@ -0,0 +1,16 @@
+import { BaseComponent } from './base.component';
+
+export class DealsPo extends BaseComponent {
+  pageUrl = 'todaysdeals';
+  pageTitle = 'h1';
+  sortDropdown = '.a-dropdown-prompt';
+  dealOfTheDayFilter = '[data-value="DEAL_OF_THE_DAY"]';
+  productCard = '.dealTile';
+  dealOfDayLabel = '.dotdBadge';
+
+  isAllProdLabelled(productsSel: string, labelSel: string) {
+    cy.get(productsSel)
+      .each($productTile => cy.wrap($productTile)
+        .should('to.have.descendants', labelSel));
+  }
+}
